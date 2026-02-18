94443660af67c53ab3cad0ff85488632bef66b75
nblinpro <nicolas.blin.pro1@gmail.com>
2026-02-18 09:54:42 +0100

    Update base path in Vite configuration
    Set the base path for the project to '/webfolio'.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // REMPLACE 'nom-de-ton-depot' par le nom exact de ton projet sur GitHub
+  base: '/webfolio', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
