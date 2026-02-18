adf99b801bfb827c2e8dc8d26b0fb384dccf07ac
nblinpro <nicolas.blin.pro1@gmail.com>
2026-02-18 09:58:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // REMPLACE 'nom-de-ton-depot' par le nom exact de ton projet sur GitHub
-  base: '/webfolio', 
+  base: '/webfolio/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
